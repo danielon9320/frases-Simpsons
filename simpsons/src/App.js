5fe337f1244503504d5177d4da1e45aafc5267a2
danielon9320 <daniel_ls93@live.com.ar>
2021-04-04 01:13:51 -0300

    app finalizada, se agrego useeffect, usestate y fetchapi

--- simpsons/src/App.js
+++ simpsons/src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Frase from "./components/Frase";
+import Spinner from "./components/Spinner";
 
 function App() {
+  const [fraseSimpsons, setFraseSimpsons] = useState({});
+  const [loader, setLoader] = useState(false);
+
+  useEffect(() => {
+    consultarAPI();
+  }, []);
+
+  const consultarAPI = async () => {
+    setLoader(true);
+    const api = await fetch("https://thesimpsonsquoteapi.glitch.me/quotes");
+    const resp = await api.json();
+    console.log(api);
+    console.log(resp[0]);
+    setTimeout( () =>{
+      setFraseSimpsons(resp[0]);
+      setLoader(false);
+    }, 1000)
+   
+  };
+
+  const cargarComponente = loader ? (
+    <Spinner></Spinner>
+  ) : (
+    <Frase fraseSimpsons={fraseSimpsons}></Frase>
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <section className="container text-center my-5">
+      <article className="d-flex flex-column align-items-center">
+        <img
+          src={process.env.PUBLIC_URL + "/logoS.png"}
+          className="w-75"
+          alt="logo"
+        ></img>
+        <button
+          className="btn btn-warning w-50 shadow my-5"
+          onClick={() => consultarAPI()}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Obtener Frase
+        </button>
+      </article>
+      {cargarComponente}
+    </section>
   );
 }
 
